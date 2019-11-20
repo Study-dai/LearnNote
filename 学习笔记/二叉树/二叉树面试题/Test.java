5e5810bad7ad4bc4f2653122f804aa637b48e885
Study-dai <1215461373@qq.com>
2019-11-20 13:31:37 +0800

    测试类

---
+++ 学习笔记/二叉树/二叉树面试题/Test.java
@@ -0,0 +1,10 @@
+package packageOJ;
+
+import package1.BinnaryTree;
+
+public class Test {
+    public static void main(String[] args) {
+        BinnaryTree binnaryTree= new BinnaryTree();
+        //TreeNode root=
+    }
+}
