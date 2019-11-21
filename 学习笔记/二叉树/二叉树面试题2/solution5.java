42b16cd01a4102abf74d4a6482fa86959e939305
Study-dai <1215461373@qq.com>
2019-11-21 23:44:03 +0800

    根据一棵树的前序遍历和中序遍历构建二叉树

---
+++ 学习笔记/二叉树/二叉树面试题2/solution5.java
@@ -0,0 +1,21 @@
+package packageOJ2;
+//根据一棵树的前序遍历和中序遍历构建二叉树
+//前序遍历：
+//中序遍历：
+public class solution5 {
+    public TreeNode buildTree(int [] preorder,int preIndex int [] inorder,int inbegin,int inend){
+
+        TreeNode root=new TreeNode(preorder[preIndex]);
+        //找到root在中序遍历数组当中的位置
+        int inorder=
+        root.left=
+        root.right=
+    }
+    public int findInorderIndex(int [] inorder,int inbegin,int inend,int )
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length == 0 || inorder.length == 0) {
+            return
+        }
+    }
+}
