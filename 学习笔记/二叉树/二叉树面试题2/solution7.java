5adf60d750b96522324495cf192861432972ac33
Study-dai <1215461373@qq.com>
2019-11-22 22:37:36 +0800

    二叉树创建字符串

---
+++ 学习笔记/二叉树/二叉树面试题2/solution7.java
@@ -0,0 +1,17 @@
+package packageOJ2;
+//二叉树创建字符串
+public class solution7 {
+    //如果左树和右树都为空什么都不做   左空右不空 （）  左不空右空
+
+    public void tree2strChild(TreeNode t,StringBuffer sb){
+        if(t==null){
+            return;
+        }
+        sb.append(t.val);
+        if(  )
+    }
+
+    public String tree2str(TreeNode t) {
+
+    }
+}
