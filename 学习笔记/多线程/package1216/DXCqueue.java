4866359a7cbb7bb299dd37d49262f2a7b519efab
Study-dai <1215461373@qq.com>
2019-12-16 22:37:43 +0800

    未完成queue

---
+++ 学习笔记/多线程/package1216/DXCqueue.java
@@ -0,0 +1,13 @@
+package psckage1216;
+
+
+public class DXCqueue {
+   private static  class Node{
+        private int val;
+        private int next;
+    }
+    private Node head=null;
+   public void put(int val){
+
+   }
+}
