649eb953169503b2a1702ee277f13e330e2f2f01
Study-dai <1215461373@qq.com>
2019-12-27 19:39:30 +0800

    两个字符串

---
+++ 做题笔记/两个字符串.java
@@ -0,0 +1,20 @@
+package package1215;
+
+import java.util.Scanner;
+/*
+//输入两个字符串，从第二个字符串中删除第一个字符串中所有的字符。
+public class Main3 {
+    public static StringBuffer func(StringBuffer sc1,StringBuffer sc2){
+        for (int i = 0; i <sc2.length(); i++) {
+            for (int j = 0; j < sc1.length(); j++) {
+
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc1=new Scanner(System.in);
+        Scanner sc2=new Scanner(System.in);
+        System.out.println(func(sc1.nextLine(),sc2.nextLine()));
+    }
+}
+*/
